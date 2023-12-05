fix allocated memory size for lp and width arrays and an indexing issue in lineshape calculation. ctypes in python now throwing an error, has decided it no longer knows where GSL is

genetic_algorithm.py
@@ -77,10 +77,10 @@ def initialise_individual(rng, init_type):
         a12  = np.zeros(ns, dtype=np.float64)
         for i in range(ns):
             n_p[i] = rng.integers(*constants.bounds['n_p'])
-            lp1[i]  = rng.uniform(*constants.bounds['lp'])
-            w1[i]   = rng.uniform(*constants.bounds['w'])
-            lp2[i]  = rng.uniform(*constants.bounds['lp'])
-            w2[i]   = rng.uniform(*constants.bounds['w'])
+            lp1[i]  = rng.uniform(*constants.bounds['lp1'])
+            w1[i]   = rng.uniform(*constants.bounds['w1'])
+            lp2[i]  = rng.uniform(*constants.bounds['lp2'])
+            w2[i]   = rng.uniform(*constants.bounds['w2'])
             a12[i]  = rng.choice(constants.bounds['a12'])
         return constants.genome(nb, ns, n_p, lp1, w1, lp2, w2, a12)
 

main.py
@@ -20,6 +20,7 @@ if __name__ == "__main__":
             ctypes.c_double, ctypes.POINTER(ctypes.c_uint),
             ctypes.POINTER(ctypes.c_double),
             ctypes.POINTER(ctypes.c_double),
+            ctypes.POINTER(ctypes.c_double),
             ctypes.c_uint, ctypes.c_uint, ctypes.c_uint,
             ctypes.POINTER(ctypes.c_double),
             ctypes.POINTER(ctypes.c_double)]
@@ -104,9 +105,9 @@ if __name__ == "__main__":
                     n_s = population[j].n_s
                     side = (n_b * n_s) + 2
                     n_p   = np.ctypeslib.as_ctypes(np.zeros(n_s + 1, dtype=np.uint32))
-                    lp    = np.ctypeslib.as_ctypes(np.zeros(n_s + 1, dtype=np.float64))
-                    width = np.ctypeslib.as_ctypes(np.zeros(n_s + 1, dtype=np.float64))
-                    a12   = np.ctypeslib.as_ctypes(np.zeros(n_s + 1, dtype=np.uint32))
+                    lp    = np.ctypeslib.as_ctypes(np.zeros(2 * (n_s + 1), dtype=np.float64))
+                    width = np.ctypeslib.as_ctypes(np.zeros(2 * (n_s + 1), dtype=np.float64))
+                    a12   = np.ctypeslib.as_ctypes(np.zeros(n_s + 1, dtype=np.float64))
                     n_p[0]   = constants.rc_params[0]
                     lp[0]    = constants.rc_params[1]
                     width[0] = constants.rc_params[2]