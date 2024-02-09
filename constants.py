change lp to be a straight offset, and mutation width to be a scaling of the total range of each parameter. implement tournament selection, fix histograms to get actual peak locations. runs fine. unsure about algorithm behaviour yet, running atm

constants.py
@@ -20,14 +20,15 @@ max_gen = 1000
 n_runs = 3
 fitness_cutoff = 0.2 # fraction of individuals kept
 d_recomb = 0.25 # random perturbation of values during crossover
-mu_width = 0.25 # width of Gaussian/Poisson we draw from for mutation
+mu_width = 0.10 # width of Gaussian/Poisson we draw from for mutation
 mu_rate = 0.05
+tourney_k = 10 # selection tournament size
 hist_snapshot = 50 # generate histograms every hist_snapshot generations
 hist_sub_max = 10 # number of subunits to make histograms for
 bounds = {'n_b': np.array([1, 12], dtype=np.int32),
           'n_s': np.array([1, 100], dtype=np.int32),
           'n_p': np.array([1, 100], dtype=np.int32),
-          'lp': np.array([200.0, 1400.0]),
+          'lp': np.array([-50.0, 50.0]),
           'pigment': np.array(["bchl_a\x00", "chl_a\x00", "chl_b\x00",
                             "chl_d\x00", "chl_f\x00", "r_apc\x00",
                             "r_pc\x00", "r_pe\x00"])}

main.py
@@ -71,6 +71,7 @@ if __name__ == "__main__":
                 avgs.fill(0.0)
                 avgsq.fill(0.0)
                 nlw_pop.fill(0.0)
+                fit_max = 0.0
                 for j, p in enumerate(population):
                     nu_phi = la.antenna(l, ip_y, p, False, False)
                     p.nu_e  = nu_phi[0]
@@ -78,6 +79,9 @@ if __name__ == "__main__":
                     # nu_phi[2] is the limit at low intensity,
                     # which is what we actually want, I think
                     p.phi_f = nu_phi[2]
+                    if (ga.fitness(p) > fit_max):
+                        fit_max = ga.fitness(p)
+                        best = population[j]
                     if nu_phi[2] < 0.0:
                         with open(filenames['neg'], "a") as f:
                             f.write(str(population[j]))
@@ -130,7 +134,7 @@ if __name__ == "__main__":
                 print(f"<n_b>     = {avgs[6]:10.4n}\t<n_b^2>       = {avgsq[6]:10.4n}\tσ = {std_dev[6]:10.4n}")
                 print(f"<n_s>     = {avgs[7]:10.4n}\t<n_s^2>       = {avgsq[7]:10.4n}\tσ = {std_dev[7]:10.4n}")
 
-                survivors, best = ga.selection(rng, population)
+                survivors = ga.selection(rng, population)
                 avg_nb_surv = np.sum(np.array([s.n_b
                                         for s in survivors]) / len(survivors))
                 avg_ns_surv = np.sum(np.array([s.n_s

stats.py
@@ -20,6 +20,7 @@ def hist(population, gen, run, temp):
     l_bin_size = 1.0
     s_max = constants.hist_sub_max
     n_pop = constants.population_size
+    # this needs changing to be e.g. 200-1400, now i've changed lp
     l_b = constants.bounds['lp']
     lbins = np.linspace(*l_b,
             num=np.round(((l_b[1] - l_b[0]) / l_bin_size)).astype(int))
@@ -34,8 +35,10 @@ def hist(population, gen, run, temp):
     for j, p in enumerate(population):
         for i in range(s_max):
             if i < p.n_s:
-               l_arr[i][j] = p.lp[i]
-               p_arr[i][j] = p.pigment[i]
+                par = constants.pigment_data[p.pigment[i]]
+                # p.lp[i] is the offset now!
+                l_arr[i][j] = p.lp[i] + par['lp'][0]
+                p_arr[i][j] = p.pigment[i]
     lh = [lbins[:-1]] # len(lbins) = len(hist) + 1
     ph = [pvals]
     for i in range(s_max):