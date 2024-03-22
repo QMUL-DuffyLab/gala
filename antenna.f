cc2badc78bf65627f0fc47cbf0ebbf95ccc2a0a6
callum <callum.gray.10@ucl.ac.uk>
2024-03-22 11:28:55 +0000

    add pigment_data JSON file. continue writing fortran kernel

--- antenna.f
+++ antenna.f
@@ -37,13 +37,13 @@ module antenna
         * (x(1 + 1:n - 0) - x(1 + 0:n - 1)))/2
     end function
 
-    function lineshape(pigment, peak_offset, json, l, lsize)&
-        result(line)
+    subroutine lineshape(pigment, peak_offset, line, lp1, json, l, lsize)
       type(json_file), intent(in) :: json
       real, dimension(lsize), intent(in) :: l
       real, intent(in) :: peak_offset
       character(len=*), intent(in) :: pigment
       real, dimension(lsize), intent(out) :: line
+      real, intent(out) :: lp1
       real, dimension(:), allocatable :: lp, width, amp
       integer :: n_gauss, i
       logical :: found
@@ -59,6 +59,7 @@ module antenna
       call json%get(pigment // '.amp', amp, found)
       if (.not.found) stop 1
       lp = lp - peak_offset
+      lp1 = lp(1)
       line = 0.0
       do i = 1, n_gauss
         line = line + amp(i) * exp(-(l - lp(i))**2/(2.0 * width(i)**2))
@@ -71,7 +72,7 @@ module antenna
       res = integrate(l, line1 * line2)
     end function overlap
 
-    pure function dG(l1, l2, n, T) result(res)
+    pure function gibbs(l1, l2, n, T) result(res)
       real, intent(in) :: l1, l2, T
       integer, intent(in) :: n
       real :: h12, s12
@@ -79,7 +80,7 @@ module antenna
       h12 = hcnm * ((l1 - l2) / (l1 * l2))
       s12 = -kb * log(n)
       res = h12 - (s12 * T)
-    end function dG
+    end function gibbs
     
     function construct_k(n_b, n_s, n_p, peak_offset, pigment, l, ip_y, lsize)&
         result(k)
@@ -89,17 +90,41 @@ module antenna
     character, dimension(10, n_s), intent(in) :: pigment
     real, dimension(lsize), intent(in) :: l, ip_y
     real, dimension(:, :), allocatable :: twa, k
-    integer :: i, j
-    real, dimension(n_s + 1, lsize) :: lines(n_s + 1, lsize)
+    real, dimension(n_s + 1, lsize) :: lines
+    real, dimension(n_s + 1) :: lps
     real, dimension(n_s) :: g
     real, dimension(2 * n_s) :: k_b
     real, dimension(lsize) :: fp_y
+    integer :: i, j, pen
+    real :: de, n, dg
+
+    fp_y = (ip_y * l) / hcnm
 
     do i = 1, n_s + 1
-      ! lineshape and gamma calc - careful of RC
+      ! lineshape and gamma calc
+      call lineshape(trim(adjustl(pigment(i))), peak_offset(i),&
+        lines(i), lps(i), json, l, lsize)
+      if (i.gt.1) then
+        g(i - 1) = n_p(i) * sigma * overlap(fp_y, lines(i), l, lsize)
+      end if
     end do
     do i = 1, n_s
       ! overlap and dG calc, fill k_b
+      de = overlap(lines(i), lines(i + 1), l, lsize)
+      n = 1.0 * n_p(i) / n_p(i + 1)
+      ! need a separate peak function to get the actual peaks for this
+      dg = gibbs(lps(i), lps(i + 1), n, T)
+      ! and need k_params argument for this
+      if (i.eq.1) then
+        rate = k_params()
+      else
+        rate = k_params()
+      end if
+      rate = rate * de
+      k_b(2 * i) = rate
+      k_b((2 * i) + 1) = rate
+      pen = merge(2 * i, (2 * i) + 1, dg > 0.0)
+      k_b(pen) = k_b(pen) * exp((-1.0 * sign(dg)) * dg / (T * kb))
     end do
 
     side = (n_b * n_s) + 2
