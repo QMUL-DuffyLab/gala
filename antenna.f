f9618dc9a7a490b622ecb69f16aa67dcbd1b7d98
callum <callum.gray.10@ucl.ac.uk>
2024-03-26 11:25:44 +0000

    working on fortran kernel still. add lmin and lmax checks when plotting antenna spectra

--- antenna.f
+++ antenna.f
@@ -1,5 +1,6 @@
 module antenna
   use json_module
+  use nnls
   implicit none
   character(len=*), parameter :: pigment_file = "pigment/pigment_data.json"
   private
@@ -82,14 +83,18 @@ module antenna
       res = h12 - (s12 * T)
     end function gibbs
     
-    function construct_k(n_b, n_s, n_p, peak_offset, pigment, l, ip_y, lsize)&
-        result(k)
+    function construct_k(n_b, n_s, n_p, peak_offset, pigment,&
+        k_params, temp, l, ip_y, lsize) result(k)
+    ! k_params = (k_diss, k_trap, k_con, k_hop, k_lhc_rc)
     integer, intent(in) :: n_b, n_s, lsize
+    real, intent(in) :: temp
     integer, dimension(n_s), intent(in) :: n_p
     real, dimension(n_s), intent(in) :: peak_offset
+    real, dimension(5), intent(in) :: k_params
     character, dimension(10, n_s), intent(in) :: pigment
     real, dimension(lsize), intent(in) :: l, ip_y
     real, dimension(:, :), allocatable :: twa, k
+    real, dimension(:), allocatable :: b, p_eq
     real, dimension(n_s + 1, lsize) :: lines
     real, dimension(n_s + 1) :: lps
     real, dimension(n_s) :: g
@@ -112,13 +117,14 @@ module antenna
       ! overlap and dG calc, fill k_b
       de = overlap(lines(i), lines(i + 1), l, lsize)
       n = 1.0 * n_p(i) / n_p(i + 1)
-      ! need a separate peak function to get the actual peaks for this
+      ! need temperature argument as well
       dg = gibbs(lps(i), lps(i + 1), n, T)
       ! and need k_params argument for this
       if (i.eq.1) then
-        rate = k_params()
+        ! in practice these two are the same number
+        rate = k_params(5)
       else
-        rate = k_params()
+        rate = k_params(4)
       end if
       rate = rate * de
       k_b(2 * i) = rate
@@ -130,6 +136,61 @@ module antenna
     side = (n_b * n_s) + 2
     allocate(twa(2 * side, 2 * side))
     allocate(k((2 * side) + 1, 2 * side))
+    ! careful of indexing here - 1-based vs 0-based
+    twa(2, 1) = k_params(3) ! k_con
+    twa(3, 1) = k_params(1) ! k_diss
+    twa(3, 2) = k_params(2) ! k_trap
+    twa(4, 2) = k_params(1)
+    twa(4, 3) = k_params(3)
+    do j = 5, 2 * side + 1, 2 * n_s
+    ! outer loop - fill RC - branch rates
+    twa(3, j)     = k_b(1)
+    twa(j, 3)     = k_b(2)
+    twa(4, j + 1) = k_b(1)
+    twa(j + 1, 4) = k_b(2)
+    do i = 1, n_s
+    ! inner loop - decay and transfer rates along branch
+    ind = j + (2 * i)
+    twa(ind, 1)       = k_params(1)
+    twa(ind + 1, 2)   = k_params(1)
+    twa(ind + 1, ind) = k_params(3)
+    if (i.gt.1) then
+      twa(ind, ind - 2)     = k_b((2 * i) + 1)
+      twa(ind + 1, ind - 1) = k_b((2 * i) + 1)
+    end if
+    if (i.lt.n_s) then
+      twa(ind, ind + 2)     = k_b(2 * (i + 1))
+      twa(ind + 1, ind + 3) = k_b(2 * (i + 1))
+    end if
+    twa(1, ind)     = g(i)
+        twa(2, ind + 1) = g(i)
+      end do
+    end do
+
+    do i = 1, 2 * side
+      do j = 1, 2 * side
+        ! assign k from twa
+        k(i, j) = twa(j, i)
+        k(i, i) = k(i, i) - twa(i, j)
+      end do
+      k(2 * side + 1, i) = 1.0
+    end do
+    allocate(b((2 * side) + 1), source = 0.0)
+    allocate(p_eq((2 * side) + 1), source = 0.0)
+    b((2 * side) + 1) = 1.0
+    p_eq = nnls(k, b)
+
+    ! check indexing here - 1-based vs 0-based
+    do i = 1, side
+      n_eq(1) = n_eq(1) + p_eq((2 * i) + 1)
+      if (i.gt.1) then
+        n_eq(i) = p_eq(2 * i) + p_eq((2 * i) + 1)
+      end if
+    end do
+    nu_e = k_params() * n_eq(1)
+    phi_e_g = nu_e / (nu_e + (k_params() * sum(n_eq(2:))))
+
+    ! now do the low gamma one
     do j = 5, 2 * side + 1, 2 * n_s
       ! outer loop - fill RC - branch rates
       do i = 1, n_s
@@ -137,11 +198,5 @@ module antenna
       end do
     end do
 
-    do i = 1, 2 * side
-      do j = 1, 2 * side
-      ! assign k from twa
-      end do
-      k(2 * side + 1, i) = 1.0
-    end do
     end function construct_k
 end module antenna
